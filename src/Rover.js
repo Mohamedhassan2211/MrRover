069819a289a82c9fc5bcacdd38bd2fa2f6facb05
mhassan112003@gmail.com <mhassan112003@gmail.com>
2023-08-30 21:46:38 +0300

    updated mars rover

--- src/Rover.js
+++ src/Rover.js
@@ -11,6 +11,7 @@ function calculateMove(move, bordDim) {
   return ((move % bordDim) + bordDim) % bordDim;
 }
 
+
 class Rover extends Component {
   constructor(props) {
     super(props);
@@ -24,8 +25,8 @@ class Rover extends Component {
           direction: 0
         },
       ],
-      obstacle:[],
-      point1:[],
+      obstacle: [],
+      point1: [],
       stepNumber: 0
     };
   }
@@ -49,21 +50,209 @@ class Rover extends Component {
   }
   point1() {
     let input = document.getElementById("123");
-    console.log([input.value.split(" ")[0], input.value.split(" ")[1]]);
-    this.setState({
-      point1: [ ...this.state.point1, [parseInt(input.value.split(" ")[0]), parseInt(input.value.split(" ")[1])]],
-     })
-    }
-  obstacle(){
-      let input = document.getElementById("234");
-      console.log([input.value.split(" ")[0], input.value.split(" ")[1]]);
+    // console.log([input.value.split(" ")[0], input.value.split(" ")[1]]);
+
+    if ((this.state.history[0].position[0] != input.value.split(" ")[0]) || (this.state.history[0].position[1] != input.value.split(" ")[1])) {
       this.setState({
-        obstacle: [ ...this.state.obstacle, [parseInt(input.value.split(" ")[0]), parseInt(input.value.split(" ")[1])]],
+        point1: [...this.state.obstacle, [parseInt(input.value.split(" ")[0]), parseInt(input.value.split(" ")[1])]],
       })
     }
-    readmessage(message){
-      return console.log(message);
+  }
+
+  search() {
+    let history = this.state.history;
+    let currentState = history[history.length - 1];
+
+    var moveDir = [1, 1]
+    let pos
+    var props = this.props;
+    pos = currentState.position.map(function (p, idx) {
+      return calculateMove(
+        p + roverTurnOptions[currentState.direction][idx] * moveDir[idx],
+        props.dimensions[idx]
+      );
+    });
+let dir=(this.state.position=[0,7]);
+if(pos[0]!=dir[0] || pos[1]!= dir[1]){
+   if(this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1])){
+    this.setState({
+      history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+    });
+  }else{
+    this.setState({
+      history: [{ move: "R", position: pos, direction: currentState.direction }]
+    });
+  }
+  }
+  else if(pos[0] === dir[0] && pos[1] === dir[1]){
+    let newDirection = calculateMove(
+      currentState.direction + (moveDir=== "R" ? -1 : 1),
+      4
+    );
+    this.setState({
+      history:[{move:"F",position: currentState.position,direction: newDirection}]
+    });
+  }
+  
+  
+
+
+  }
+
+// while(this.state.point1[0] !== pos[0]){
+//       if(this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1])){
+//         this.setState({
+//           history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+//         });
+//       }
+//       else
+//       this.setState({
+//         history: [{ move: "F", position: pos, direction: currentState.direction }]
+//       }
+//       );
+//       //pos=this.state.history[0].position;
+//   }
+
+      // if(this.state.point1[0] === pos[0] && this.state.point1[1] === pos[1]){
+      //   this.setState({
+      //     history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+      //   });
+      // }
+      // else {
+
+      // pos =currentState.position.map(function (p, idx) {
+      //   return calculateMove(
+      //     p + roverTurnOptions[currentState.direction][idx] * moveDir[idx],
+      //     props.dimensions[idx]
+      //   );
+      // });
+       
+
+
+
+
+
+        // if(this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1])){
+        //   this.setState({
+        //       history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+        //   })
+        // }else{
+        //   this.setState({
+        //       history: [{ move: "F", position: [0, 1], direction: currentState.direction }]
+        //   })
+        //   if(this.state.point1.some((i) => i[0] === position[0, 2] )){
+        //     this.setState({
+        //         history: [{ move: "F", position: [0, 1], direction: currentState.direction }]
+        //     })
+        //   }
+        //   else{
+        //     this.setState({
+        //         history: [{ move: "F", position: [0, 2], direction: currentState.direction }]
+        //     })
+        //     }
+        //    }
+        
+
+
+  //   let count =0
+    
+  //   while(this.state.point1[0] !== pos[0] && this.state.point1[1] !== pos[1]) {
+  //     if(this.state.point1[0] === pos[0] && this.state.point1[1] === pos[1]){
+  //       this.setState({
+  //         history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+  //       });
+  //     }
+  //     else {
+  //       console.log([{ move: "F", position: [0, 1 + (count)], direction: currentState.direction }])
+  //       this.setState({
+  //         history: [{ move: "F", position: [0, 1 +pos[0]], direction: currentState.direction }]
+  //       });
+        
+
+  //       //pos=this.state.history[0].position;
+  //       count ++
+  //       pos[0]=count;
+  //     if(count >= 5)  
+  //       break
+  //   }  
+    
+  // };
+
+    // let count = 0
+    // while (this.state.point1.some((i) => i[1] != pos[1])) {
+    //   console.log("test search", this.state.point1 , " <<<<< ",this.state.point1.some((i) => i[1] != pos[1]))
+    //   //   this.setState({
+    //   //   history: [{move:"F", position : pos , direction: currentState.direction}]
+    //   // });   
+    //   console.log(this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1]))
+    //   if (this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1])) {
+    //     this.setState({
+    //       history: [{ move: "F", position: currentState.position, direction: currentState.direction }]
+    //     });
+
+    //   } else {
+    //     console.log([{ move: "F", position: [0, 1 + (count / 2)], direction: currentState.direction }])
+    //     this.setState({
+    //       history: [{ move: "F", position: [0, 1 + count], direction: currentState.direction }]
+    //     });
+    //     count++
+    //     history = this.state.history;
+    //     currentState = history[history.length - 1];
+    //     pos = currentState.position.map(function (p, idx) {
+    //       return calculateMove(
+    //         p + roverTurnOptions[currentState.direction][idx] * moveDir[idx],
+    //         props.dimensions[idx]
+    //       );
+    //     });
+    //     // this.setState({
+    //     //   history: [{ move: "F", position: pos, direction: currentState.direction }]
+    //     // })
+    //     if (count > 5)
+    //       break
+
+    //   }
+    // }
+
+
+
+    // const history = this.state.history;
+    // const currentState = history[history.length - 1];
+    // var moveDir = [1, 1] ;
+    // var props = this.props;
+    // const pos = currentState.position.map(function(p, idx) {
+    //   return calculateMove(
+    //     p + roverTurnOptions[currentState.direction][idx] * moveDir[idx],
+    //     props.dimensions[idx]
+    //   );
+    // });
+    // this.setState({
+    //   history: history.concat([
+    //     {
+    //       move: move,
+    //       position: pos,
+    //       direction: currentState.direction
+    //     }
+    //   ])
+    // });
+
+  obstacle() {
+    let input = document.getElementById("234");
+    // console.log([input.value.split(" ")[0], input.value.split(" ")[1]]); 
+    // console.log(x,y)
+    // console.log(this.state)
+    if ((this.state.history[0].position[0] != input.value.split(" ")[0]) || (this.state.history[0].position[1] != input.value.split(" ")[1])) {
+      this.setState({
+        obstacle: [...this.state.obstacle, [parseInt(input.value.split(" ")[0]), parseInt(input.value.split(" ")[1])]],
+      })
     }
+
+    // this.setState({
+    //   obstacle: [ ...this.state.obstacle, [parseInt(input.value.split(" ")[0]), parseInt(input.value.split(" ")[1])]],
+    // })
+  }
+  readmessage(message) {
+    return console.log(message);
+  }
 
   move(move) {
     const history = this.state.history;
@@ -74,16 +263,14 @@ class Rover extends Component {
 
       var props = this.props;
 
-      const pos = currentState.position.map(function(p, idx) {
+      const pos = currentState.position.map(function (p, idx) {
         return calculateMove(
           p + roverTurnOptions[currentState.direction][idx] * moveDir[idx],
           props.dimensions[idx]
         );
       });
-      if(this.state.obstacle.some((i)=>i[0]===currentState.position[0] && i[1]===currentState.position[1])){
-        return;
-      }
-      else if(this.state.obstacle.some((i)=>i[0]===pos[0] && i[1]===pos[1])){
+
+      if (this.state.obstacle.some((i) => i[0] === pos[0] && i[1] === pos[1])) {
         this.setState({
           history: history.concat([
             {
@@ -94,19 +281,30 @@ class Rover extends Component {
           ])
         });
       }
-    else{
-      this.setState({
-        history: history.concat([
-          {
-            move: move,
-            position: pos,
-            direction: currentState.direction
-          }
-        ])
-      });
+      else if (this.state.point1.some((i) => i[0] === pos[0] && i[1] === pos[1])) {
+        this.setState({
+          history: history.concat([
+            {
+              move: move,
+              position: currentState.position,
+              direction: currentState.direction
+            }
+          ])
+        });
+      }
+      else {
+        this.setState({
+          history: history.concat([
+            {
+              move: move,
+              position: pos,
+              direction: currentState.direction
+            }
+          ])
+        });
       }
     }
-   else if (move === "L" || move === "R") {
+    else if (move === "L" || move === "R") {
       let newDirection = calculateMove(
         currentState.direction + (move === "R" ? 1 : -1),
         4
@@ -136,62 +334,71 @@ class Rover extends Component {
     const current = history[this.state.stepNumber];
 
     return (
-    <div className="rover">
-      <div className="container">
-       <div className="header">
-        <h1>MrRover</h1>
-       </div>
-       <div className="board">
-        
-          <Board
-            obstacle={this.state.obstacle}
-            point1={this.state.point1}
-            dimensions={this.props.dimensions}
-            position={current.position}
-            direction={current.direction}
-          />
-          <div className="board-1">
-          <br></br>
-          <div>{this.readmessage}</div>
-            <label className="label">pleace enter the cordinte of obstacle(x,y)</label>
-            <br></br>
-            
-            <input type="text" id="234" />
-            <button onClick={ () => this.obstacle()}>
-              add obstacle 
-            </button>
+      <div className="rover">
+        <div className="container">
+          <div className="header">
+            <h1>MrRover</h1>
           </div>
-          <div className="board-1">
-          <br></br>
-            <label className="label">pleace enter the cordinte of point(x,y)</label>
-            <br></br>
-            <input type="text" id="123" />
-            <button onClick={ () => this.point1()}>
-              add point
-            </button>
+          <div className="board">
+
+            <Board
+              obstacle={this.state.obstacle}
+              point1={this.state.point1}
+              dimensions={this.props.dimensions}
+              position={current.position}
+              direction={current.direction}
+            />
+            <div className="board-1">
+              <br></br>
+              <div>{this.readmessage}</div>
+              <label className="label">pleace enter the cordinte of obstacle(x,y)</label>
+              <br></br>
+
+              <input type="text" id="234" />
+              <button onClick={() => this.obstacle()}>
+                add obstacle
+              </button>
+            </div>
+            <div className="board-1">
+              <br></br>
+              <label className="label">pleace enter the cordinte of mark(x,y)</label>
+              <br></br>
+              <input type="text" id="123" />
+              <button
+                onClick={() => this.point1()}>
+                add point
+              </button>
+            </div>
+            <div className="board-1">
+              <button
+                className="input"
+                // disabled={!this.checkForMove()}
+                onClick={() => this.search("f")}>
+                Search
+              </button>
+            </div>
           </div>
-       </div>
-       <div className="controls">
-       <br></br>
-          {<div>Commands: {this.state.inputString}</div>}
-          <br></br>
+          <div className="controls">
+            <br></br>
+            {<div>Commands: {this.state.inputString}</div>}
+            <br></br>
 
-          <Button action="L" onClick={i => this.queueMove(i)} />
-          <Button action="R" onClick={i => this.queueMove(i)} />
-          <Button action="F" onClick={i => this.queueMove(i)} />
-          <Button action="B" onClick={i => this.queueMove(i)} />
+            <Button action="L" onClick={i => this.queueMove(i)} />
+            <Button action="R" onClick={i => this.queueMove(i)} />
+            <Button action="F" onClick={i => this.queueMove(i)} />
+            <Button action="B" onClick={i => this.queueMove(i)} />
 
-          <Button
-            action="RUN"
-            disabled={!this.checkForMove()}
-            onClick={() => this.executeNextMove()}
-          />
-          <br></br>
-          <br></br>
-          <br></br>
-       </div>
+            <Button
+              action="RUN"
+              disabled={!this.checkForMove()}
+              onClick={() => this.executeNextMove()}
+            />
+            <br></br>
+            <br></br>
+            <br></br>
+          </div>
+        </div>
       </div>
-     </div>
     );
   }
 }
